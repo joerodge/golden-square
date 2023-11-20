fda6b9d2591d05272de565b98ea7957399d2d6d8
Joe Rodgers <joerodgers89@outlook.com>
2023-11-20 19:24:45 +0000

    Added extra integration test

--- codebases/mocking_bites/tests/test_integration_tasks.py
+++ codebases/mocking_bites/tests/test_integration_tasks.py
@@ -39,3 +39,21 @@ def test_task_formatter_with_complete_task():
     task_1.mark_complete()
     task_formatter = TaskFormatter(task_1)
     assert task_formatter.format() == "- [x] Walk the dog"
+
+
+def test_task_format_with_multiple_tasks_in_task_list():
+    task_list = TaskList()
+    task_1 = Task("Walk the dog")
+    task_2 = Task("Walk the cat")
+    task_3 = Task("Feed the elephant")
+    task_list.add(task_1)
+    task_list.add(task_2)
+    task_list.add(task_3)
+    task_1.mark_complete()
+    task_3.mark_complete()
+    assert ([TaskFormatter(task).format() for task in task_list.all()]
+            == ["- [x] Walk the dog",
+                "- [ ] Walk the cat",
+                "- [x] Feed the elephant"
+                ]
+            )
